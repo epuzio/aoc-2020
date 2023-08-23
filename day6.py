b60361113b3863af7653d7b30c5b2c918b55a065
Esmé Puzio <esmepuzio@169-231-111-7.wireless.ucsb.edu>
2023-08-23 16:47:11 -0700

    Day 6 done, revise part 1 to match pt 2?

--- day6.py
+++ day6.py
@@ -1,41 +1,28 @@
-with open('day6.txt') as f: #used groups strategy I saw on subreddit
+# Part 2
+
+with open('day6.txt') as f:
     groups = f.read().split('\n\n')
-    print(groups)
     total = 0
     for g in groups:
-        s = set()
-        for c in g.replace('\n', ''):
-            s.add(c)
-            print(c)
-            print(s)
+        s = "abcdefghijklmnopqrstuvwxyz"
+        answers = g.split('\n')
+        for a in answers:
+            print(a)
+            s = set(s) & set(a)
         total +=len(s)
-        print("total: ", total) 
     print(total)
 
 
 
 
+# # Part 1
 
-
-
-
-
-
-#doesnt work imma fix later tho
-
-# with open('day6.txt') as f:
-#     total = 0
-#     s = set()
-#     for line in f:
-#         if line.strip() == '':
-#             total +=len(s)
-#             print("set ", s)
-#             s.clear()
-#         else:
-#             for c in enumerate(line.strip()):
-#                 print(c)
-#                 s.add(c)
-#         print("total: ", total)
-#     total +=len(s)    
-#     print(total)
-
+with open('day6.txt') as f: 
+    groups = f.read().split('\n\n')
+    total = 0
+    for g in groups:
+        s = set()
+        for c in g.replace('\n', ''):
+            s.add(c)
+        total +=len(s)
+    print(total)
