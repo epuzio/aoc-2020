006d3faf02ebedb4cc23db97c4d4a0e9c4173301
Esmé Puzio <esmepuzio@Esmes-MBP.attlocal.net>
2023-08-29 00:03:04 -0700

    Note added for day 9, https://stackoverflow.com/questions/35608015/algorithm-to-find-a-consecutive-sub-sequence-whose-sum-would-be-a-asked-number-m pointer chasing

--- day9.py
+++ day9.py
@@ -21,7 +21,7 @@ def contiguous_range(lines, invalid_num): #finding start and end of invalid rang
     start = 0 # starting index of range
     end = 0 # ending index of range
     sum = 0
-    while start <= len(lines) and end <= len(lines): # O(2n) solution
+    while start <= len(lines) and end <= len(lines): # O(2n) solution (pointer chasing)
         if sum < invalid_num:
             sum += int(lines[end])
             end += 1
@@ -38,6 +38,3 @@ pt2 = lines[start_idx:end_idx]
 
 print("Part 1:", pt1)
 print("Part 2:", int(max(pt2))+int(min(pt2)))
-
-
-
