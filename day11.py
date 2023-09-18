08f1a39fbea35dd98902d4dccbdf69c3c81b2b2c
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-09-18 15:19:02 +0100

    Day 11 works for sample input, not for puzzle input

--- day11.py
+++ day11.py
@@ -2,28 +2,23 @@
 # Definitely a cleaner way to do this, but adding a 'border' of seats (labeled N) around the
 # plane helps avoid out-of-bound errors w/ indexing, thank you Lokshtanov!
 
-#when u resume - there is something fucky w the indexing
-
 lines = open('day11.txt').readlines()
 prev_state = []
 curr_state = []
-prev_state.append(['N' for _ in range(len(lines[0]))]) #adding seat border
+prev_state.append(['N' for _ in range(len(lines[0])+1)]) #adding seat border
 for i, line in enumerate(lines):
-    prev_state.append(['N'])
+    prev_state.append(['N']) #seat border - clean this
     for c in line.strip():
         prev_state[-1].append(c)
-    prev_state[-1].append('N') 
-prev_state.append(['N' for _ in range(len(line))]) #adding seat border
-print(prev_state, curr_state)
+    prev_state[-1].append('N') #seat border
+prev_state.append(['N' for _ in range(len(lines[0])+1)]) #adding seat border
 
 # Part 1
-curr_state = [['N' for _ in range(len(prev_state[0]))] for _ in range(len(prev_state))] #initialize
-count = 0
-while count < 3:
-    count+=1
+while True: #terrible practice, but oh well
+    curr_state = [['N' for _ in range(len(prev_state[0]))] for _ in range(len(prev_state))] #initialize
     for r, row in enumerate(prev_state):
         for s, seat in enumerate(row):
-            if r in range(1, len(lines)) and s in range(1, len(lines)):
+            if r in range(1, len(lines[0])) and s in range(1, len(lines[0])):
                 neighbors = (
                     prev_state[r-1][s-1] + prev_state[r-1][s] + prev_state[r-1][s+1] +
                     prev_state[r][s -1] + prev_state[r][s +1] +
@@ -36,10 +31,14 @@ while count < 3:
                     curr_state[r][s] = 'L'
                 else:
                     curr_state[r][s] = prev_state[r][s]
+    if prev_state == curr_state:
+        print("SAME")
+        break
     prev_state = curr_state
-    curr_state = [['N' for _ in range(len(prev_state[0]))] for _ in range(len(prev_state))] #initialize
-    print("\nprev", prev_state)
-    print("\ncurr", curr_state)
-    
+
+print(sum(seat.count('#') for seat in prev_state))
+
+
+
 
     
